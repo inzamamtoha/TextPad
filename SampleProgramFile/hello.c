e881aafa3d2bf7d0a11421a1b6d613c56bcfbd46
al2helal <al2helal@gmail.com>
2017-11-03 22:51:36 +0600

    delete unneccessary file

--- SampleProgramFile/hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Roni vai assise, roni vai ke biri dao"
-	return 0;
-}
--- SampleProgramFile/sample.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-using namespace std; 
-
-int main() 
-{ 
-	cout << "This is a sample C++ code"; 
-	return 0; 
-}
